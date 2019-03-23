21b3693df816a38c791b29c66327082b6af20ead
jpclark6 <jpclark6@gmail.com>
2019-03-23 11:39:25 -0600

    Adds test for creating link

--- spec/api/v1/link_endpoints_spec.rb
+++ spec/api/v1/link_endpoints_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe 'Links API' do
-  it 'can get a list of links' do
-    Link.create(link: 'wolf', slug: 'wolf')
-    Link.create(link: 'bear', slug: 'bear')
-    Link.create(link: 'tiger', slug: 'tiger')
+  Link.create(link: 'wolf', slug: 'wolf')
+  Link.create(link: 'bear', slug: 'bear')
+  Link.create(link: 'tiger', slug: 'tiger')
 
+  it 'can get a list of links' do
     get '/api/v1/links'
 
     expect(response).to be_successful
@@ -19,5 +19,19 @@ describe 'Links API' do
     expect(link_data[:data][2][:type]).to eq('links')
     expect(link_data[:data][2][:attributes][:link]).to eq('tiger')
     expect(link_data[:data][2][:attributes][:slug]).to eq('tiger')
+  end
+
+  it 'can create a link' do
+    link_name = 'hawk'
+
+    post '/api/v1/links', params: { link: link_name }
+
+    expect(response).to be_successful
+    link_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(link_data[:data].length).to eq(1)
+    expect(link_data[:data][0][:type]).to eq('links')
+    expect(link_data[:data][0][:attributes][:link]).to eq('hawk')
+    expect(link_data[:data][0][:attributes][:slug]).to eq('hawk')
   end
 end
